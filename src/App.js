75dd21fd8ea31cc7fd5f247a9aaad7fa9dfadc7e
Amit J Lilhare <119440651+AmitJL@users.noreply.github.com>
2023-03-23 21:03:16 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Resturant from "./Component/Basics/Resturant";
+// // import UseState from "./component/Hooks/useState";
+// // import UseEffect from "./component/Hooks/useEffect";
+// import UseReducer from "./Component/Hooks/useReduser";
+
+const App = () => {
+  return <Resturant />;
+};
+
+export default App;
